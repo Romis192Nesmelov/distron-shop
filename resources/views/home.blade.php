ed7cf592e518c6b71b56cb8afc9ddaf23a7d982f
Romis <romis@nesmelov.com>
2024-01-31 12:21:59 +0300

    everything has been swapped

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,15 @@
     </x-section>
     <hr>
     <x-section wow_delay=".1" wow_direction="Left" data-scroll-destination="services" head="{{ $services->name }}">
+        <x-row class="d-flex justify-content-center pt-5">
+            @foreach($services->items as $k => $item)
+                @include('blocks.type_item_block',[
+                    'col' => 4,
+                    'addClass' => 'ms-lg-3 me-lg-3',
+                    'showDescription' => true
+                ])
+            @endforeach
+        </x-row>
         <div class="row mb-4">
             @for ($i=1;$i<=4;$i++)
                 <div class="col-md-3 col-sm-4 wow animate__animated animate__fadeInUp" data-wow-offset="10" data-wow-delay="{{ ($i + 1) * 0.2 }}s">
@@ -25,15 +34,6 @@
             @endfor
         </div>
         {!! $services->text !!}
-        <x-row class="d-flex justify-content-center pt-5">
-            @foreach($services->items as $k => $item)
-                @include('blocks.type_item_block',[
-                    'col' => 4,
-                    'addClass' => 'ms-lg-3 me-lg-3',
-                    'showDescription' => true
-                ])
-            @endforeach
-        </x-row>
     </x-section>
     <x-section class="color color1" wow_delay=".1" data-scroll-destination="{{ str()->slug($content[0]->head) }}" head="{{ $content[0]->head }}">
         <x-row>
