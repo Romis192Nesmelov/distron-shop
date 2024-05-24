f024074944c84cc19a39bd7271d24c738c96bcb4
Romis <romis@nesmelov.com>
2024-05-24 11:40:48 +0300

    fixed type page

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-//use Illuminate\Http\Request;
 use App\Models\Action;
 use App\Models\Contact;
 use App\Models\Content;
--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -10,35 +10,37 @@
     <x-section>
         <x-row>
             <div class="col-lg-3 col-sm-12 pe-lg-4 pe-sm-0">
-                <form id="filters" method="get" action="{{ route('items',['slug' => $type->slug]) }}">
-                    @foreach ($filters as $name => $params)
-                        @if ($params['min'] != $params['max'])
-                            @include('blocks.filter_slider_block',[
-                                'mt' => !$loop->first,
-                                'name' => $name,
-                                'minVal' => $params['min_val'],
-                                'maxVal' => $params['max_val']
-                            ])
-                        @endif
-                    @endforeach
-                    <div class="w-100 mt-4 text-center">
-                        @include('blocks.button_block', [
-                            'addClass' => 'w-100',
-                            'primary' => true,
-                            'buttonType' => 'submit',
-                            'icon' => 'icon-filter4',
-                            'buttonText' => trans('content.filtered')
-                        ])
-                        <a href="{{ route('items',['slug' => $type->slug]) }}">
+                @if (count($filters))
+                    <form id="filters" method="get" action="{{ route('items',['slug' => $type->slug]) }}">
+                        @foreach ($filters as $name => $params)
+                            @if ($params['min'] != $params['max'])
+                                @include('blocks.filter_slider_block',[
+                                    'mt' => !$loop->first,
+                                    'name' => $name,
+                                    'minVal' => $params['min_val'],
+                                    'maxVal' => $params['max_val']
+                                ])
+                            @endif
+                        @endforeach
+                        <div class="w-100 mt-4 text-center">
                             @include('blocks.button_block', [
-                                'addClass' => 'w-100 mt-2',
+                                'addClass' => 'w-100',
                                 'primary' => true,
-                                'icon' => 'icon-reset',
-                                'buttonText' => trans('content.reset_filter')
+                                'buttonType' => 'submit',
+                                'icon' => 'icon-filter4',
+                                'buttonText' => trans('content.filtered')
                             ])
-                        </a>
-                    </div>
-                </form>
+                            <a href="{{ route('items',['slug' => $type->slug]) }}">
+                                @include('blocks.button_block', [
+                                    'addClass' => 'w-100 mt-2',
+                                    'primary' => true,
+                                    'icon' => 'icon-reset',
+                                    'buttonText' => trans('content.reset_filter')
+                                ])
+                            </a>
+                        </div>
+                    </form>
+                @endif
             </div>
             <div class="items items-type-{{ $type->id }} col-lg-9 col-sm-12 row">
                 @foreach($items as $k => $item)
